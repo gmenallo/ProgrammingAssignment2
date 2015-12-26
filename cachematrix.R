ce3ee724f88e84086c94f423077a21378700ef2e
gmenallo <giorgio.menallo@gmail.com>
2015-12-26 18:35:51 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The following functions 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+# creates a cacheble matrix
+
+makeCacheMatrix <- function(x = matrix()) {       
+  inv <- NULL                                      # sets the initial value of the inverse matrix
+  set <- function(y) {                             # sets the value of the original and inverse matrix in the global environmet  
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x                              # creates function to return value of original matrix when called in global environment
+  setsolve <- function(solve) inv <<- solve        # creates function to set value of inverse matrix when called in global environment
+  getsolve <- function() inv                       # creates function to return value of inverse matrix when called in global environment
+  list(set = set, get = get,                       # constructs a list of function to be called in the global environment
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+# checks if exist a cached value and return it or computes and saves a new one
+
+cacheSolve <- function(x, ...) {                    
+  inv <- x$getsolve()                              # loads inverse matrix from cache
+  if(!is.null(inv)) {                              # if inverse matrix is not NULL, returns value of inv
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()                                  # gets original matrix from cache
+  inv <- solve(data, ...)                          # computes inverse matrix
+  x$setsolve(inv)                                  # saves inv in cache
+  inv                                              # print inverse matrix
 }
